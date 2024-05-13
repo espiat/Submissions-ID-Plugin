12aa92091815252d4f1cd41bc8bc4d59aec71572
Marcus <m.spanier@espiat.comm>
2024-05-13 17:47:57 +0200

    refactor: Add submission ID to email content in Elementor form

--- Submissions-ID.php
+++ Submissions-ID.php
@@ -1,39 +1,82 @@
 <?php
 
 /**
- * @package Submissions ID by Schwarzer Falke e.V.
+ * @package Elementor Submissions ID in Email by der-spanier.de
  * @version 1.0.0
  */
 /*
-Plugin Name: Submissions ID
-Plugin URI: https://schwarzer-falke.org/
-Description: Provides the submission ID for integration into HTML.
-Author: Schwarzer Falke e.V. - Christian Geschwandtner
-Version: 1.0.0
-Author URI: https://schwarzer-falke.org/
+Plugin Name: Elementor Submissions ID in Email
+Plugin URI: https://der-spanier.de
+Description: Provides the submission ID as appendix AND Shortcode [get_submission_id] for email Template to the email sent by Elementor Pro forms.
+Author: Marcus Spanier
+Version: 1.1.0
+Author URI: https://der-spanier.de
 */
 
-// Define a class for your functionality
+/**
+ * Class My_Elementor_Customizations
+ *
+ * This class provides customizations for Elementor Pro forms.
+ */
 class My_Elementor_Customizations
 {
+    /**
+     * @var string The name of the submissions table.
+     */
+    private $submissions_table;
 
-    // Constructor to initialize hooks
+    /**
+     * My_Elementor_Customizations constructor.
+     *
+     * Initializes the class by setting up hooks and properties.
+     */
     public function __construct()
     {
-        add_filter('elementor_pro/forms/wp_mail_message', array($this, 'add_custom_text_to_email'), 10, 3);
+        global $wpdb;
+        $this->submissions_table = $wpdb->prefix . 'e_submissions';
+
+        add_action('init', array($this, 'register_get_submission_id_shortcode'));
+        add_filter('elementor_pro/forms/wp_mail_message', array($this, 'add_custom_text_to_email'), 10, 1);
     }
 
-    // Method to add custom text to the email
+    /**
+     * Registers the 'get_submission_id' shortcode.
+     */
+    public function register_get_submission_id_shortcode()
+    {
+        add_shortcode('get_submission_id', array($this, 'get_submission_id'));
+    }
+
+    /**
+     * Retrieves the highest submission ID from the submissions table.
+     *
+     * @return int|string The highest submission ID or 'No results found' if no submissions exist.
+     */
+    public function get_submission_id()
+    {
+        global $wpdb;
+
+        $sql    = "SELECT MAX(ID) as max_id FROM `{$this->submissions_table}`;";
+        $result = $wpdb->get_var($sql);
+
+        return $result ? (int) $result : 'No results found';
+    }
+
+    /**
+     * Adds custom text to the email message.
+     *
+     * @param string $message The original email message.
+     *
+     * @return string The modified email message with the custom text appended.
+     */
     public function add_custom_text_to_email($message)
     {
-        // Modify $message as needed, adding your custom text
+        $submission_id = $this->get_submission_id();
+
         $custom_text = "----------------------------- This is custom text added to the email.\n";
+        $custom_text .= "Submission ID: " . $submission_id . "\n";
 
-        // Append the custom text to the existing message
-        $message .= $custom_text;
-
-        // Return the modified message
-        return $message;
+        return $message . $custom_text;
     }
 }
 
